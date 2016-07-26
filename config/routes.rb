f5e26d959625327a72550a05c072a5760a7fcd09
Bradley Wittenbrook <bradley.wittenbrook@gmail.com>
2016-07-25 22:52:36 -0400

    Mount the ember application with ember-cli-rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'application#index'
+
+  Rails.application.routes.draw do
+    mount_ember_app :frontend, to: "/"
+  end
 end
